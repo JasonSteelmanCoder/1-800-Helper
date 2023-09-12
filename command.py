dc6928ae4492cd1caa9707240d43ee9ca7a0ebb4
Jason Steelman <jason.steelman@sposatogse.org>
2023-09-12 10:40:45 -0400

    Split DisplayFrame up into 4 classes: itself, plus one for each page that it displays--Chat, Search, and AvailableWords

--- command.py
+++ command.py
@@ -9,12 +9,12 @@ import word_checker as wc
 import tkinter as tk
 from chatgpt_api_caller import APICall
 
-class Search():
+class SearchFrame():
     def __init__(self, search_term, event=None):
         self.search_term = search_term
 
     def find_word_for_search_term(self):
-        app.display_frame.clear_search_results()
+        self.clear_search_results()
         desired_num = wc.find_num_for_word(self.search_term)
 
         app.display_frame.desired_num_label = tk.Label(app.display_frame, text=f"You are looking for a number ending with {desired_num}.")
@@ -24,88 +24,35 @@ class Search():
             number_found = False
             if phone_number.endswith(desired_num):
                 number_found = True
-                app.display_frame.display_search_results(phone_number)
+                self.display_search_results(phone_number)
                 break
         if number_found == False:
-            app.display_frame.display_search_results(None)
+            self.display_search_results(None)
 
-class DisplayFrame(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__()
-        self.configure(height=400, width=450, highlightbackground="black", highlightthickness=1)
-        self.pack_propagate(False)
-        self.pack(side=tk.RIGHT)
-
-    def show_search_window(self):
-        self.clear_display()
-        search_directions = tk.Label(self, text="Enter a word here to see if it is available.\nNote: words must be 3-7 letters.")
-        search_directions.pack(pady=40)
-        self.search_box = tk.Entry(self)
-        self.search_box.pack(padx=20, pady=0)
-        self.search_box.focus_set()
-        self.search_box.bind('<Return>', lambda event=None: self.start_search(event))
-
-    def start_search(self, event=None):
-        search = Search(self.search_box.get())
-        search.find_word_for_search_term()
-
-    def show_some_available_words(self):
-        self.clear_display()
-        self.display_wait_message()
-        available_combos = {}
-        for num in app.number_retrieval.get_available_phone_nums_short(app.available_numbers_long):
-            prepared_num = wc.prepare_phone_number(num)
-            temp_words = wc.find_words_for_num(prepared_num)
-            if temp_words:
-                temp_words.sort(key=len, reverse=True)
-                available_combos[num] = temp_words
-        formatted_output = self.format_output(available_combos)
-        available_combos_label = tk.Label(self, text=formatted_output, justify=tk.LEFT)
-        available_combos_label.pack(pady=20)
-        self.destroy_wait_message()
-
-    def display_wait_message(self):
-        self.wait_message = tk.Label(self, text="Finding available words...")
-        self.wait_message.pack()
-        self.update()
-
-    def destroy_wait_message(self):
-        self.wait_message.destroy()
-        self.update()
-
-    def clear_display(self):
-        leftovers = self.winfo_children()
-        for widget in leftovers:
-            widget.destroy()
-        self.update()
-
-    def format_output(self, combos:dict) -> str:
-        combo_list = []
-        for item in combos.items():
-            combo_list.append(str(item))
-        combo_list.sort(key=len, reverse=True)
-        combo_str = "\n".join(combo_list)
-        return combo_str
-    
     def display_search_results(self, number_results):
         if number_results:      # When the return is not None
-            search_results = tk.Label(self, text=f"The number you are looking for is available! It's {number_results}")
+            search_results = tk.Label(app.display_frame, text=f"The number you are looking for is available! It's {number_results}")
             search_results.pack()
         else:
-            sorry_message = tk.Label(self, text="Sorry. That number is not available.\nTry another word, OR\nPress the button on the left to see words that are available.")
+            sorry_message = tk.Label(app.display_frame, text="Sorry. That number is not available.\nTry another word, OR\nPress the button on the left to see words that are available.")
             sorry_message.pack()
 
     def clear_search_results(self):
-        for i in range(len(self.winfo_children())-1, -1, -1):
+        for i in range(len(app.display_frame.winfo_children())-1, -1, -1):
             if i > 1:
-                self.winfo_children()[i].destroy()
+                app.display_frame.winfo_children()[i].destroy()
+
+class ChatFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master=master
 
     def show_chat_screen(self):
-        self.clear_display()
-        self.chat_directions = tk.Label(self, pady=20, text="Write a short description of your organization.\nInclude aspects that you would like to highlight in your marketing.\nThen press enter.")
+        app.display_frame.clear_display()
+        self.chat_directions = tk.Label(app.display_frame, pady=20, text="Write a short description of your organization.\nInclude aspects that you would like to highlight in your marketing.\nThen press enter.")
         self.chat_directions.pack()
         example_text = "Example: At Those Who Wander Travel Agency, we'll never get you lost."
-        self.chat_box = tk.Text(self, wrap="word")
+        self.chat_box = tk.Text(app.display_frame, wrap="word")
         self.chat_box.config(height=10, width=50)
         self.chat_box.pack()
         self.chat_box.insert(tk.END, example_text)
@@ -118,8 +65,8 @@ class DisplayFrame(tk.Frame):
         self.display_chat_results(api_call.prepare_suggestions())
 
     def display_chat_results(self, suggestions:list):
-        self.clear_display()
-        self.suggestions_header = tk.Label(self, text="ChatGPT Says:  Number:  Availability\n", font=("courier", 9))
+        app.display_frame.clear_display()
+        self.suggestions_header = tk.Label(app.display_frame, text="ChatGPT Says:  Number:  Availability\n", font=("courier", 9))
         self.suggestions_header.pack(anchor=tk.W)
         if suggestions:
             for word in suggestions:
@@ -132,12 +79,75 @@ class DisplayFrame(tk.Frame):
                     else:
                         availability_message = "Not available."
                         font_color = "grey"
-                    label = tk.Label(self, text=f"{word}{' '*(15-len(word))}{wc.find_num_for_word(word)}{' '*(9-len(word))}{availability_message}", fg=font_color, font=("courier", 9))
+                    label = tk.Label(app.display_frame, text=f"{word}{' '*(15-len(word))}{wc.find_num_for_word(word)}{' '*(9-len(word))}{availability_message}", fg=font_color, font=("courier", 9))
                     label.pack(anchor=tk.W)
         else:
-            self.error_label = tk.Label(self, text="Sorry! Something went wrong. Please try again.")
+            self.error_label = tk.Label(app.display_frame, text="Sorry! Something went wrong. Please try again.")
             self.error_label.pack()
 
+class ShowSomeAvailableWordsFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master=master
+        
+    def show_some_available_words(self):
+        app.display_frame.clear_display()
+        self.display_wait_message()
+        available_combos = {}
+        for num in app.number_retrieval.get_available_phone_nums_short(app.available_numbers_long):
+            prepared_num = wc.prepare_phone_number(num)
+            temp_words = wc.find_words_for_num(prepared_num)
+            if temp_words:
+                temp_words.sort(key=len, reverse=True)
+                available_combos[num] = temp_words
+        formatted_output = self.format_some_available_words(available_combos)
+        available_combos_label = tk.Label(app.display_frame, text=formatted_output, justify=tk.LEFT)
+        available_combos_label.pack(pady=20)
+        self.destroy_wait_message()
+
+    def display_wait_message(self):
+        self.wait_message = tk.Label(app.display_frame, text="Finding available words...")
+        self.wait_message.pack()
+        self.update()
+
+    def destroy_wait_message(self):
+        self.wait_message.destroy()
+        self.update()
+
+    def format_some_available_words(self, combos:dict) -> str:
+        combo_list = []
+        for item in combos.items():
+            combo_list.append(str(item))
+        combo_list.sort(key=len, reverse=True)
+        combo_str = "\n".join(combo_list)
+        return combo_str
+
+class DisplayFrame(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__()
+        self.configure(height=400, width=450, highlightbackground="black", highlightthickness=1)
+        self.pack_propagate(False)
+        self.pack(side=tk.RIGHT)
+
+    def clear_display(self):
+        leftovers = self.winfo_children()
+        for widget in leftovers:
+            widget.destroy()
+        self.update()
+
+    def instantiate_search(self, event=None):
+        self.search = SearchFrame(self.search_box.get())
+        self.search.find_word_for_search_term()
+
+    def show_search_window(self):
+        self.clear_display()
+        search_directions = tk.Label(self, text="Enter a word here to see if it is available.\nNote: words must be 3-7 letters.")
+        search_directions.pack(pady=40)
+        self.search_box = tk.Entry(self)
+        self.search_box.pack(padx=20, pady=0)
+        self.search_box.focus_set()
+        self.search_box.bind('<Return>', lambda event=None: self.instantiate_search(event))
+    
 class MenuFrame(tk.Frame):
     def __init__(self, master=None):
         super().__init__()
@@ -152,11 +162,11 @@ class MenuFrame(tk.Frame):
         self.find_num_button.config(width=24, height=2)
         self.find_num_button.pack(pady=10)
 
-        self.suggest_word_button = tk.Button(self, text="Suggest some words.\n(Powered by ChatGPT)", command=lambda: app.display_frame.show_chat_screen())
+        self.suggest_word_button = tk.Button(self, text="Suggest some words.\n(Powered by ChatGPT)", command=lambda: app.chat_frame.show_chat_screen())
         self.suggest_word_button.config(width=24)
         self.suggest_word_button.pack(pady=10)
 
-        self.find_word_button = tk.Button(self, text="Show me some available words.", command=lambda: app.display_frame.show_some_available_words())
+        self.find_word_button = tk.Button(self, text="Show me some available words.", command=lambda: app.show_some_available_words_frame.show_some_available_words())
         self.find_word_button.config(width=24, height=2)
         self.find_word_button.pack(pady=10)
 
@@ -168,6 +178,8 @@ class MainApp(tk.Tk):
 
         self.menu_frame = MenuFrame(master=self)
         self.display_frame = DisplayFrame(master=self)
+        self.chat_frame = ChatFrame(master=self.display_frame)
+        self.show_some_available_words_frame = ShowSomeAvailableWordsFrame(master=self.display_frame)
 
         self.number_retrieval = anf.NumberRetrieval()
         self.available_numbers_long = self.number_retrieval.get_available_phone_nums_long()
@@ -177,9 +189,6 @@ if __name__ == "__main__":
     app.mainloop()
 
 # TODO:
-# - make short list of available nums a subset of the long list 
-# - break display_frame into more manageable classes for each page
-
 # - add layer where users can select the number they want to buy
 # - make available chat display labels clickable
 # - clean up display format for list of available numbers
